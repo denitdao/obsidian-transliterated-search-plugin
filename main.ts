a964542687c03d89cf5fad71d8772e0c45eeb088
Denys <dodenisa@gmail.com>
2023-01-28 00:25:25 +0200

    MVP created - ability to transliterate Ukrainian and russial chars - settings to choose language - button to enable the plugin

--- main.ts
+++ main.ts
@@ -1,26 +1,65 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import CyrillicToTranslit from 'cyrillic-to-translit-js';
+import { App, Editor, MarkdownView, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface TransliteratedSearchPluginSettings {
+	ukrainian: boolean;
+	russian: boolean;
+	defaultState: boolean;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'lang'
+const DEFAULT_SETTINGS: TransliteratedSearchPluginSettings = {
+	ukrainian: true,
+	russian: true,
+	defaultState: false
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class TransliteratedSearchPlugin extends Plugin {
+	settings: TransliteratedSearchPluginSettings;
 
 	async onload() {
 		await this.loadSettings();
 
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+		const ribbonIconEl = this.addRibbonIcon('languages', 'Transliterated Search Plugin', (evt: MouseEvent) => {
+			// console.log('plugin: ', (this.app as any).internalPlugins.getPluginById("global-search"));
+
+			const searchPanel = this.app.workspace.getLeavesOfType('search')[0]?.view;
+			// console.log('searchComponent: ', searchPanel.searchComponent);
+
+			searchPanel.searchComponent.translitRU = this.settings.russian ? CyrillicToTranslit({ preset: 'ru' }) : null;
+			searchPanel.searchComponent.translitUK = this.settings.ukrainian ? CyrillicToTranslit({ preset: 'uk' }) : null;
+
+			// Overriding existing global-search function
+			searchPanel.searchComponent.getValue = function () {
+				// console.log('original input: ', this.inputEl.value);
+				const userInput: string = this.inputEl.value ?? "";
+				if (userInput == "") return "";
+
+				const termSet: Set<string> = new Set();
+				termSet.add(userInput);
+
+				if (this.translitRU) {
+					const ru_en = this.translitRU.transform(userInput).toLowerCase();
+					const en_ru = this.translitRU.reverse(userInput).toLowerCase();
+					termSet.add(ru_en);
+					termSet.add(en_ru);
+				}
+				if (this.translitUK) {
+					const uk_en = this.translitUK.transform(userInput).toLowerCase();
+					const en_uk = this.translitUK.reverse(userInput).toLowerCase();
+					termSet.add(uk_en);
+					termSet.add(en_uk);
+				}
+				const transliteratedInput = Array.from(termSet).join(" OR ");
+
+				console.log('transliterated input: ', transliteratedInput);
+
+				return transliteratedInput;
+			}
+
+			new Notice('Transliteration is enabled!');
 		});
+
 		// Perform additional things with the ribbon
 		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
@@ -28,14 +67,6 @@ export default class MyPlugin extends Plugin {
 		const statusBarItemEl = this.addStatusBarItem();
 		statusBarItemEl.setText('Status Bar Text');
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
 			id: 'sample-editor-command',
@@ -45,33 +76,35 @@ export default class MyPlugin extends Plugin {
 				editor.replaceSelection('Sample Editor Command');
 			}
 		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new TransliteratedSearchSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+		/* this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
+			const targetEl = evt.target;
+
+			if (!(targetEl instanceof HTMLElement)) {
+				return;
+			}
+
+			if (targetEl instanceof HTMLInputElement) {
+				const searchPanel = this.app.workspace.getLeavesOfType('search')[0]?.view;
+
+				if (searchPanel.containerEl.contains(targetEl)) {
+					if (!this.isBuiltInElementToOpenFile(targetEl)){
+						return;
+					}
+					console.log('input: ', targetEl.value);
+					console.log('targetEl:', targetEl);
+					console.dir(targetEl);
+				}
+			}
+		}); */
+
+		// event on search bar input
+		this.registerDomEvent(document, 'input', (evt: InputEvent) => {
+			// const searchPanel = this.app.workspace.getLeavesOfType('search')[0]?.view;
+			// console.log('search query: ', searchPanel.searchQuery);
 		});
 
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
@@ -88,49 +121,56 @@ export default class MyPlugin extends Plugin {
 
 	async saveSettings() {
 		await this.saveData(this.settings);
+		const searchPanel = this.app.workspace.getLeavesOfType('search')[0]?.view;
+		searchPanel.searchComponent.translitRU = this.settings.russian ? CyrillicToTranslit({ preset: 'ru' }) : null;
+		searchPanel.searchComponent.translitUK = this.settings.ukrainian ? CyrillicToTranslit({ preset: 'uk' }) : null;
 	}
+
+/* 	
+	searchComponent - div class="search-input-container"
+	                    var o = this.searchComponent.getValue()
+	                  , r = this.app.internalPlugins.getPluginById("global-search");
+	               var i = this.info.app.internalPlugins.getPluginById("global-search");
+	               i && i.instance.openGlobalSearch("tag:" + n)
+	q: how to run n.startSearch() from the internalPlugins.getPluginById("global-search")?
+	a: i.instance.startSearch()    ----  are they close to each other 
+*/
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class TransliteratedSearchSettingTab extends PluginSettingTab {
+	plugin: TransliteratedSearchPlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: TransliteratedSearchPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', { text: 'Settings for transliterated search plugin.' });
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+			.setName('Transliterate Ukrainian')
+			.setDesc('If checked, the plugin will transliterate Ukrainian characters.')
+			.addToggle(toggle => toggle
+				.setValue(this.plugin.settings.ukrainian)
 				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
+					console.log('Transliterate Ukrainian: ' + value);
+					this.plugin.settings.ukrainian = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('Transliterate russian')
+			.setDesc('If checked, the plugin will transliterate russian characters.')
+			.addToggle(toggle => toggle
+				.setValue(this.plugin.settings.russian)
+				.onChange(async (value) => {
+					console.log('Transliterate russian: ' + value);
+					this.plugin.settings.russian = value;
 					await this.plugin.saveSettings();
 				}));
 	}
