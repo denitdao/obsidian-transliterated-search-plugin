c2da4610b2313309326c141f99ba50477d524c95
Denys <dodenisa@gmail.com>
2023-01-28 17:23:13 +0200

    Improved functionality: - plugin activates on startup - code refactored - ribbon icon focus - more notifications and refactored namings

--- README.md
+++ README.md
@@ -3,11 +3,16 @@
 This is a plugin for Obsidian (https://obsidian.md) that allows you to search non latin characters in Obsidian. 
 It uses the transliteration library to convert non latin characters to latin characters. 
 
-For example, if you search for \"Джон\" it will return all the files that contain \"Джон\" or \"John\".
+For example, if you search for \"Мастер\" it will return all the files that contain \"Мастер\" or \"Master\".
 
 **NOTE:** It's usefult for everyone who has Obsidian notes in multiple languages.
 
 ### TODO:
-[] Add vice versa transliteration (from latin to non latin)
-[] Add support for other languages
-[] Add language translation configuration
+- [x] Add language translation configuration
+- [x] Add two-sided transliteration (from latin to non latin and vice versa)
+- [ ] Add support for more languages
+	- [x] Ukrainian
+	- [x] russian
+	- [ ] Chinese
+- [ ] Extend with more clever transliteration rules
+- [ ] Clean up search field smarter
--- main.ts
+++ main.ts
@@ -1,118 +1,54 @@
-import CyrillicToTranslit from 'cyrillic-to-translit-js';
-import { App, Editor, MarkdownView, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import cyrillicToTranslit from 'cyrillic-to-translit-js';
+import { App, Notice, Plugin, PluginSettingTab, SearchComponent, Setting, View } from 'obsidian';
 
 interface TransliteratedSearchPluginSettings {
 	ukrainian: boolean;
 	russian: boolean;
-	defaultState: boolean;
+	turnOnByDefault: boolean;
 }
 
 const DEFAULT_SETTINGS: TransliteratedSearchPluginSettings = {
 	ukrainian: true,
 	russian: true,
-	defaultState: false
+	turnOnByDefault: true,
 }
 
 export default class TransliteratedSearchPlugin extends Plugin {
 	settings: TransliteratedSearchPluginSettings;
+	statusBarItemEl: HTMLElement;
+	ribbonIconEl: HTMLElement;
+	isTransliterationEnabled: boolean;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('languages', 'Transliterated Search Plugin', (evt: MouseEvent) => {
-			// console.log('plugin: ', (this.app as any).internalPlugins.getPluginById("global-search"));
-
-			const searchPanel = this.app.workspace.getLeavesOfType('search')[0]?.view;
-			// console.log('searchComponent: ', searchPanel.searchComponent);
-
-			searchPanel.searchComponent.translitRU = this.settings.russian ? CyrillicToTranslit({ preset: 'ru' }) : null;
-			searchPanel.searchComponent.translitUK = this.settings.ukrainian ? CyrillicToTranslit({ preset: 'uk' }) : null;
-
-			// Overriding existing global-search function
-			searchPanel.searchComponent.getValue = function () {
-				// console.log('original input: ', this.inputEl.value);
-				const userInput: string = this.inputEl.value ?? "";
-				if (userInput == "") return "";
-
-				const termSet: Set<string> = new Set();
-				termSet.add(userInput);
-
-				if (this.translitRU) {
-					const ru_en = this.translitRU.transform(userInput).toLowerCase();
-					const en_ru = this.translitRU.reverse(userInput).toLowerCase();
-					termSet.add(ru_en);
-					termSet.add(en_ru);
-				}
-				if (this.translitUK) {
-					const uk_en = this.translitUK.transform(userInput).toLowerCase();
-					const en_uk = this.translitUK.reverse(userInput).toLowerCase();
-					termSet.add(uk_en);
-					termSet.add(en_uk);
-				}
-				const transliteratedInput = Array.from(termSet).join(" OR ");
-
-				console.log('transliterated input: ', transliteratedInput);
-
-				return transliteratedInput;
-			}
-
-			new Notice('Transliteration is enabled!');
-		});
-
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
+		// This adds a settings tab so the user can configure language settings
 		this.addSettingTab(new TransliteratedSearchSettingTab(this.app, this));
 
-		/* this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			const targetEl = evt.target;
+		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
+		this.statusBarItemEl = this.addStatusBarItem();
 
-			if (!(targetEl instanceof HTMLElement)) {
-				return;
+		// This creates an icon in the left ribbon.
+		this.ribbonIconEl = this.addRibbonIcon('languages', 'Transliterated Search', (evt: MouseEvent) => {
+			if (this.isTransliterationEnabled) {
+				this.removeSettingsFromSearchComponent();
+				this.defaultGetterOfSearchComponent();
+				new Notice('Transliteration is disabled!');
+			} else {
+				this.applySettingsToSearchComponent();
+				this.overrideGetterOfSearchComponent();
+				new Notice('Transliteration is enabled!');
 			}
-
-			if (targetEl instanceof HTMLInputElement) {
-				const searchPanel = this.app.workspace.getLeavesOfType('search')[0]?.view;
-
-				if (searchPanel.containerEl.contains(targetEl)) {
-					if (!this.isBuiltInElementToOpenFile(targetEl)){
-						return;
-					}
-					console.log('input: ', targetEl.value);
-					console.log('targetEl:', targetEl);
-					console.dir(targetEl);
-				}
-			}
-		}); */
-
-		// event on search bar input
-		this.registerDomEvent(document, 'input', (evt: InputEvent) => {
-			// const searchPanel = this.app.workspace.getLeavesOfType('search')[0]?.view;
-			// console.log('search query: ', searchPanel.searchQuery);
 		});
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		// Load plugin on startup if required
+		this.observeSearchPanelLoaded();
 	}
 
 	onunload() {
-
+		// Returning everything to the default
+		this.removeSettingsFromSearchComponent();
+		this.defaultGetterOfSearchComponent();
 	}
 
 	async loadSettings() {
@@ -121,20 +57,136 @@ export default class TransliteratedSearchPlugin extends Plugin {
 
 	async saveSettings() {
 		await this.saveData(this.settings);
-		const searchPanel = this.app.workspace.getLeavesOfType('search')[0]?.view;
-		searchPanel.searchComponent.translitRU = this.settings.russian ? CyrillicToTranslit({ preset: 'ru' }) : null;
-		searchPanel.searchComponent.translitUK = this.settings.ukrainian ? CyrillicToTranslit({ preset: 'uk' }) : null;
+		this.applySettingsToSearchComponent();
 	}
 
-/* 	
-	searchComponent - div class="search-input-container"
-	                    var o = this.searchComponent.getValue()
-	                  , r = this.app.internalPlugins.getPluginById("global-search");
-	               var i = this.info.app.internalPlugins.getPluginById("global-search");
-	               i && i.instance.openGlobalSearch("tag:" + n)
-	q: how to run n.startSearch() from the internalPlugins.getPluginById("global-search")?
-	a: i.instance.startSearch()    ----  are they close to each other 
-*/
+	/**
+	 * Loads transliteration plugin on startup if required.
+	 * Uses observer to detect when the search panel is loaded.
+	 */
+	private observeSearchPanelLoaded(): void {
+		const observer = new MutationObserver(mutations => {
+			if (document.querySelector('.search-input-container')) {
+				observer.disconnect();
+				if (this.settings.turnOnByDefault) {
+					console.log("Enabling transliteration on startup");
+					this.getSearchComponent().then((searchComponent) => {
+						searchComponent.inputEl.value = "";
+					});
+					this.applySettingsToSearchComponent();
+					this.overrideGetterOfSearchComponent();
+				}
+			}
+		});
+		observer.observe(document.body, { attributes: true, childList: true, subtree: true });
+	}
+
+	/**
+	 * Overrides search component getter to add transliteration.
+	 * Method {@code getValue} is called by the core Search plugin to get the search query.
+	 * This method is overridden and appended with transliterated terms.
+	 * Enables transliteration.
+	 */
+	private overrideGetterOfSearchComponent(): void {
+		this.getSearchComponent().then((searchComponent) => {
+			searchComponent.getValue = function () {
+				const t = this as ModifiedSearchComponent;
+				const userInput: string = t.inputEl.value ?? "";
+				if (userInput == "") return "";
+
+				const termSet: Set<string> = new Set();
+				termSet.add(userInput);
+
+				if (t.translitRU) {
+					const ru_en = t.translitRU.transform(userInput).toLowerCase();
+					const en_ru = t.translitRU.reverse(userInput).toLowerCase();
+					termSet.add(ru_en);
+					termSet.add(en_ru);
+				}
+				if (t.translitUK) {
+					const uk_en = t.translitUK.transform(userInput).toLowerCase();
+					const en_uk = t.translitUK.reverse(userInput).toLowerCase();
+					termSet.add(uk_en);
+					termSet.add(en_uk);
+				}
+				const transliteratedInput = Array.from(termSet).join(" OR ");
+
+				return transliteratedInput;
+			}
+			this.updateTransliterationStatus(true);
+		});
+	}
+
+	/**
+	 * Returns the search component getter to the default.
+	 * Disables transliteration.
+	 */
+	private defaultGetterOfSearchComponent(): void {
+		this.getSearchComponent().then((searchComponent) => {
+			searchComponent.getValue = function () {
+				const t = this as ModifiedSearchComponent;
+				return t.inputEl.value;
+			}
+			this.updateTransliterationStatus(false);
+		});
+	}
+
+	/**
+	 * This function initializes search component with transliteration settings.
+	 */
+	private applySettingsToSearchComponent(): void {
+		this.getSearchComponent().then((searchComponent) => {
+			searchComponent.translitRU = this.settings.russian ? cyrillicToTranslit({ preset: 'ru' }) : undefined;
+			searchComponent.translitUK = this.settings.ukrainian ? cyrillicToTranslit({ preset: 'uk' }) : undefined;
+		});
+	}
+
+	/**
+	 * This function removes transliteration settings from search component.
+	 * This is needed to return the search component to the default state.
+	 */
+	private removeSettingsFromSearchComponent(): void {
+		this.getSearchComponent().then((searchComponent) => {
+			searchComponent.translitRU = undefined;
+			searchComponent.translitUK = undefined;
+		});
+	}
+
+	/**
+	 * Function to update the status of the transliteration.
+	 * Also updates the status bar and ribbon icon.
+	 * @param isEnabled - boolean value to set the status of the transliteration
+	 */
+	private updateTransliterationStatus(isEnabled: boolean): void {
+		this.isTransliterationEnabled = isEnabled;
+		this.updateStatusBar(isEnabled ? 'Transliteration On' : 'Transliteration Off');
+		if (isEnabled)
+			this.ribbonIconEl.addClass('is-active');
+		else
+			this.ribbonIconEl.removeClass('is-active');
+	}
+
+	private updateStatusBar(text: string): void {
+		this.statusBarItemEl.setText(text);
+	}
+
+	/**
+	 * Function to get the native searchComponent from the core Search panel
+	 * @returns searchComponent from search panel
+	 */
+	private getSearchComponent(): ModifiedSearchComponent {
+		const searchPanel: CoreSearchPanel = this.app.workspace.getLeavesOfType('search')[0]?.view as CoreSearchPanel;
+		return searchPanel.searchComponent;
+	}
+	// TODO: try to make this work in async way
+	// private getSearchComponent(): Promise<ModifiedSearchComponent> {
+	// 	return new Promise<ModifiedSearchComponent>((resolve) => {
+	// 		const searchPanel: CoreSearchPanel = this.app.workspace.getLeavesOfType('search')[0]?.view as CoreSearchPanel;
+	// 		if (searchPanel && searchPanel.searchComponent) {
+	// 			return resolve(searchPanel.searchComponent);
+	// 		}
+	// 	});
+	// }
 }
 
 class TransliteratedSearchSettingTab extends PluginSettingTab {
@@ -158,7 +210,6 @@ class TransliteratedSearchSettingTab extends PluginSettingTab {
 			.addToggle(toggle => toggle
 				.setValue(this.plugin.settings.ukrainian)
 				.onChange(async (value) => {
-					console.log('Transliterate Ukrainian: ' + value);
 					this.plugin.settings.ukrainian = value;
 					await this.plugin.saveSettings();
 				}));
@@ -169,9 +220,29 @@ class TransliteratedSearchSettingTab extends PluginSettingTab {
 			.addToggle(toggle => toggle
 				.setValue(this.plugin.settings.russian)
 				.onChange(async (value) => {
-					console.log('Transliterate russian: ' + value);
 					this.plugin.settings.russian = value;
 					await this.plugin.saveSettings();
 				}));
+
+		new Setting(containerEl)
+			.setName('Turn on by default')
+			.setDesc('If checked, the plugin will be activated each time you open obsidian. NOTE: will clear search input on startup.')
+			.addToggle(toggle => toggle
+				.setValue(this.plugin.settings.turnOnByDefault)
+				.onChange(async (value) => {
+					this.plugin.settings.turnOnByDefault = value;
+					await this.plugin.saveSettings();
+				}));
 	}
+}
+
+
+// OBSIDIAN UI COMPONENT INTERFACES
+interface CoreSearchPanel extends View {
+	searchComponent: ModifiedSearchComponent;
+}
+
+interface ModifiedSearchComponent extends SearchComponent {
+	translitRU: undefined | any;
+	translitUK: undefined | any;
 }
